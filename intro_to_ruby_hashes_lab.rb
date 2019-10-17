53692aed5d9df0f380fd755858d1d8ef88d7d1e2
Bretton B Holmes <famunkwriter@me.com>
2019-10-17 02:46:24 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,8 +15,8 @@ def id_generator
 end
 
 def my_hash_creator(key, value)
-  my_hash_creator = {1 => "key", 2 => "value"}
-  my_hash_creator[1,2]
+  my_hash_creator = {:key => "value"}
+  my_hash_creator[:key]
   end
 
 def read_from_hash(hash, key)
